9dde4879c627f9f0ae1965d5bd12a88337fcfdbb
scasare178EducaAnd <92301430+scasare178EducaAnd@users.noreply.github.com>
2022-04-19 12:45:23 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+function CreaEnemigo () {
+    Enemigo = game.createSprite(randint(0, 4), randint(0, 4))
+}
+let Enemigo: game.LedSprite = null
+basic.clearScreen()
+let Jugador = game.createSprite(0, 0)
+game.setScore(0)
+CreaEnemigo()
+basic.forever(function () {
+    if (0 > input.acceleration(Dimension.X)) {
+        Jugador.change(LedSpriteProperty.X, 1)
+    }
+    if (input.acceleration(Dimension.X) < -600) {
+        Jugador.change(LedSpriteProperty.X, -1)
+    }
+    if (input.acceleration(Dimension.Y) > 600) {
+        Jugador.change(LedSpriteProperty.Y, 1)
+    }
+    if (input.acceleration(Dimension.Y) < -600) {
+        Jugador.change(LedSpriteProperty.Y, -1)
+    }
+    if (Jugador.isTouching(Enemigo)) {
+        game.addScore(1)
+        Enemigo.delete()
+        CreaEnemigo()
+    }
+})
